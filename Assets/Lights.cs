0d1477772995a08be7f4e5dc2f134c62dca520b9
Alhynae <3dil19.prieur@ucolaval.net>
2020-05-21 14:13:04 +0200

    0.0.5.7 - Toggle_Lights

---
+++ Assets/Lights.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lights : MonoBehaviour
+{
+    public GameObject[] gameObjectsArray;
+    public GameObject globalLight;
+ 
+    public void ToggleLights(bool state) 
+    {
+        foreach(GameObject go in gameObjectsArray)
+        {
+                go.SetActive (state);   
+        }
+    }
+
+    public void ToggleGlobal(bool state) 
+    {
+        if ( globalLight.activeSelf == true){
+            globalLight.SetActive(false);
+        } else {
+            globalLight.SetActive(true);
+        }
+    }
+}
